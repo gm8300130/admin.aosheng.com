b30f8e43eab3fda8c2aa70c6d0007f51b49c18f3
Boris Lin <Boris.Lin@aosheng.local>
2017-09-04 18:01:42 +0800

    修改初始路径

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -10,7 +10,7 @@ Route::group([
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
 
-    $router->get('/', 'HomeController@index');
+    $router->get('/', 'ReportController@index');
 
     $router->get('/agentReport', 'ReportController@index');
 });
